5b59ae8ee7537cb64c35bf65b5ec10dbff59d95d
AlIna12ivanova <93904889+AlIna12ivanova@users.noreply.github.com>
2022-01-31 10:51:46 +0700

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calon_mordohka
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calon_mordohkaEntities1 context;
+        public MainWindow()
+        {
+            InitializeComponent();
+            context = new Calon_mordohkaEntities1();
+            ShowTable();
+            
+        }
+        private void ShowTable()
+        {
+            DataCalon.ItemsSource = context.ClientService2.ToList();
+        }
+
+        private void BtnAddData_Click(object sender, RoutedEventArgs e)
+        {
+            var NewClient = new ClientService2();
+            context.ClientService2.Add(NewClient);
+            var NewWind = new WindowClient(context, NewClient);
+            NewWind.ShowDialog();
+            ShowTable();
+        }
+
+        private void BtnDeleteData_Click(object sender, RoutedEventArgs e)
+        {
+            var currentClientService2 = DataCalon.SelectedItem as ClientService2;
+            if (currentClientService2 == null)
+            {
+                MessageBox.Show("Выберите строку!");
+                return;
+            }
+            MessageBoxResult messageBoxResult = MessageBox.Show("Вы хотите удалить?", "Удаление", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (messageBoxResult == MessageBoxResult.Yes)
+            {
+                context.ClientService2.Remove(currentClientService2);
+                context.SaveChanges();
+                ShowTable();
+            }
+        }
+
+        private void DataCalon_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BtnEditData_Click(object sender, RoutedEventArgs e)
+        {
+            Button BtnEdit = sender as Button;
+            var currentClientService2 = BtnEdit.DataContext as ClientService2;
+            var EditWindow = new WindowClient(context, currentClientService2);
+            EditWindow.ShowDialog();
+        }
+    }
+}
---
+++ WindowClient.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Calon_mordohka
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowClient.xaml
+    /// </summary>
+    public partial class WindowClient : Window
+    {
+        Calon_mordohkaEntities1 context;
+        public WindowClient(Calon_mordohkaEntities1 context, ClientService2 clientService2)
+        {
+            InitializeComponent();
+            CmbClient.ItemsSource = context.Clients.ToList();
+            CmbService.ItemsSource = context.Services.ToList();
+            this.DataContext = clientService2;
+        }
+
+        private void BtnSaveData_Click(object sender, RoutedEventArgs e)
+        {
+            context.SaveChanges();
+            this.Close();
+        }
+    }
+}
